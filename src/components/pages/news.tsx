6aabe99e1ccc75e24150880cb656ebb4167816ef
Gardiano <joaopaulong@hotmail.com>
2024-08-08 12:36:46 -0300

    refactor(pages/news): add useNews and usePagination hooks

--- src/components/pages/news.tsx
+++ src/components/pages/news.tsx
@@ -1,67 +1,25 @@
-import { useEffect, useState } from 'react';
-import { getNews } from '@/services/api';
-import { News } from '../models/news';
+import { useEffect } from 'react';
 import { Grid } from '../grid/grid';
 import { Paginations } from '../pagination/pagination';
 import { useParams } from 'react-router-dom';
+import { usePagination } from '../hooks/usePagination';
+import { useNews } from '../hooks/useNews';
 
 export const NewsPage = () => {
-  const [news, setNews] = useState<News[]>([]);
-  const [paginationNews, setPaginationNews] = useState<News[]>([]);
-  const [page, setPage] = useState<number>(1);
-  const [totalPages, setTotalPages] = useState<number>(1);
-  const [totalNews] = useState<number>(100);
+  const { news, fetchNews } = useNews();
+  const { paginationNews, totalPages, page, fetchNewsForPagination,
+    handlePrevPage, handleNextPage } = usePagination();
   const params = useParams();
 
   useEffect(() => {
-    fetchNews(1);
-    fetchNewsForPagination(page);
+    fetchNews(1, params.id!);
+    fetchNewsForPagination(page, params.id!);
   }, [params.id, page]);
-
-  const fetchNews = async (page: number) => {
-    const category = params.id!;
-    const itemsPerPage = 9;
-    try {
-      const response = await getNews(category, page, itemsPerPage);
-      setNews(response.data.news);
-    } catch (e) {
-      console.error('Error fetching news', e);
-    }
-  };
-
-  const fetchNewsForPagination = async (page: number) => {
-    const category = params.id!;
-    const itemsPerPage = 20;
-    try {
-      const response = await getNews(category, page, itemsPerPage);
-      if (page > 0) {
-        setPaginationNews(response.data.news.slice(9));
-        setTotalPages(Math.ceil(totalNews / itemsPerPage));
-      }
-    } catch (e) {
-      console.error('Error fetching news', e);
-    }
-  };
-
-  const handlePrevPage = () => {
-    if (page > 1) {
-      setPage(page - 1);
-    }
-  };
-  const handleNextPage = () => {
-    if (page < totalPages) {
-      setPage(page + 1);
-    }
-  };
 
   return (
     <>
       <Grid data={news} theme={params.id!} paginationData={paginationNews} />
-      <Paginations
-        nextPage={handleNextPage}
-        previousPage={handlePrevPage}
-        currentPage={page}
-        totalPages={totalPages}
+      <Paginations nextPage={handleNextPage} previousPage={handlePrevPage} currentPage={page} totalPages={totalPages}
       />
     </>
   );
