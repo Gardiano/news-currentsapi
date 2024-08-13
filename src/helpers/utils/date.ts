83cf43e0d92a3b3858d104aff0a6b2fd314c1e08
Gardiano <joaopaulong@hotmail.com>
2024-08-13 14:59:22 -0300

    refactor(date.ts): create new format date

--- src/helpers/utils/date.ts
+++ src/helpers/utils/date.ts
@@ -3,5 +3,6 @@ import 'dayjs/locale/en-gb'
 dayjs.locale('en-gb');
 
 export const Date = (date: string) => {
-  return dayjs(date).format('DD/MM/YY');
+  let day = date.slice(8,10);
+  return dayjs(date).format(`MMM ${day}, YYYY`);
 }
