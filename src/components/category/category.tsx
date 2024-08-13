3fe342328494133cac9ed7a1c142a77982780633
Gardiano <joaopaulong@hotmail.com>
2024-08-13 14:36:42 -0300

    refactor(category): add category component

--- src/components/category/category.tsx
+++ src/components/category/category.tsx
@@ -1,19 +1,18 @@
-
 interface CategoryProps {
   categories?: string[]
 }
 
 export const Category = (props: CategoryProps) => {
   return (
-   <>
-    {props.categories &&
-      <div className="w-fit h-fit flex flex-row items-start justify-start">
-        <span className="w-fit h-[fit] text-xs p-[6px] 
-        bg-transparent text-slate-500 rounded-sm border border-slate-500">
-          {props.categories}
-        </span>
-      </div> 
-    }
-   </>
+    <>
+      {props.categories &&
+        <div className="w-fit h-fit flex flex-row items-start justify-start">
+          <span className="w-fit h-[fit] text-xs p-[6px] 
+            bg-transparent text-slate-500 rounded-sm border border-slate-500">
+            {props.categories}
+          </span>
+        </div>
+      }
+    </>
   )
 }
