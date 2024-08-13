e1a8bdd3dcbce1dec13309f07f55566fa0a17697
Gardiano <joaopaulong@hotmail.com>
2024-08-13 14:39:04 -0300

    style(grid): changes to grid styles

--- src/components/grid/grid.tsx
+++ src/components/grid/grid.tsx
@@ -2,14 +2,11 @@ import { Cards } from "../card/cards"
 import { News } from "../models/news"
 import { NavigationMenu } from "../navigation/navigationMenu"
 import { Theme } from "../theme/theme"
+
 interface GridProps {
   data: News[];
   paginationData?: News[]
   theme: string;
-  previousPage?: () => void;
-  nextPage?: () => void;
-  pages?: number;
-  totalPages?: number;
 }
 
 export const Grid = (props: GridProps) => {
@@ -17,9 +14,9 @@ export const Grid = (props: GridProps) => {
     <main>
       <Theme theme={props.theme} />
       <NavigationMenu />
-      <section className="pt-0 w-full max-w-[1144px] mx-auto gap-4 
-        grid grid-cols-1 lg:grid-cols-2 sm:grid-rows-1 sm:p-4">
-        <div className='px-4 w-full h-full col-span-1'>
+      <section className="w-full max-w-[1144px] mx-auto gap-4 
+        grid grid-cols-1 lg:grid-cols-2 sm:grid-rows-1 sm:p-0">
+        <div className='px-0 w-full h-full col-span-1'>
           {props.data.slice(0, 1).map(data => (
             <Cards
               key={data.id}
@@ -30,14 +27,15 @@ export const Grid = (props: GridProps) => {
               category={data.category}
               published={data.published}
               description={data.description}
-              imageSize="w-full h-auto sm:h-[410px] sm:rounded-sm"
-              cardHeaderStyles='w-full h-full p-3 text-md sm:text-lg lg:text-xl'
-              className='w-full flex-col rounded-none pb-0 flex justify-center'
+              imageSize="w-full h-auto sm:h-[437px]
+              sm:rounded-bl-none sm:rounded-br-none sm:rounded-sm"
+              cardHeaderStyles='w-full h-full p-4 text-md sm:text-lg lg:text-xl'
+              className='w-full flex-col flex justify-center'
             />
           ))}
         </div>
 
-        <div className='px-4 w-full h-full grid grid-rows-1 sm:grid-cols-2 gap-4 
+        <div className='w-full h-full grid grid-rows-1 sm:grid-cols-2 gap-4 
         col-span-full sm:col-span-1'>
           {props.data.slice(1, 5).map(data => (
             <Cards
@@ -47,16 +45,16 @@ export const Grid = (props: GridProps) => {
               title={data.title}
               category={data.category}
               published={data.published}
-              imageSize="w-full h-full sm:w-auto sm:h-[146px] sm:rounded-sm"
-              cardHeaderStyles='w-full h-auto p-3 text-md sm:text-lg lg:text-xl'
-              className='p-0 w-full h-auto flex-col justify-start pb-0 rounded-sm
-            '
+              imageSize="w-full h-full max-h-[170px] 
+              sm:rounded-bl-none sm:rounded-br-none sm:rounded-sm"
+              cardHeaderStyles='w-full h-auto p-4 text-md sm:text-lg lg:text-xl'
+              className='w-full h-auto flex-col justify-start'
             />
           ))}
         </div>
 
-        <div className='px-4 w-full h-full gap-4 grid  grid-cols-2 col-span-full
-        md:grid-cols-4 xl:grid-cols-4'>
+        <div className='px-6 w-full h-full gap-4 grid grid-cols-2 col-span-full
+        md:grid-cols-4 xl:grid-cols-4 sm:px-0'>
           {props.data.slice(5, 13).map(data => (
             <Cards
               url={data.url}
@@ -66,15 +64,16 @@ export const Grid = (props: GridProps) => {
               title={data.title}
               published={data.published}
               cardTitleStyles="line-clamp-6"
-              imageSize="w-full h-auto sm:h-[146px] sm:rounded-sm"
-              cardHeaderStyles='w-full h-auto p-3 text-md sm:text-lg lg:text-xl'
-              className='p-2 w-full h-full flex-col rounded-none pb-0 flex justify-start sm:p-0'
+              imageSize="w-full h-auto sm:h-[146px] 
+              sm:rounded-bl-none sm:rounded-br-none sm:rounded-sm"
+              cardHeaderStyles='w-full h-auto p-4 text-md sm:text-lg lg:text-xl'
+              className='w-full h-full flex-col flex justify-start'
             />
           ))}
         </div>
 
-        <div className='px-4 w-full h-full mx-auto gap-4 
-        grid grid-cols-1 col-span-full'>
+        <div className='w-full h-full mx-auto gap-4 
+        grid grid-cols-1 col-span-full' id="paginated-items">
           {props.paginationData?.map(data => (
             <Cards
               key={data.id}
@@ -84,11 +83,12 @@ export const Grid = (props: GridProps) => {
               description={data.description}
               published={data.published}
               cardTitleStyles="w-full sm:w-[90%] line-clamp-6"
-              imageSize="w-full h-full sm:h-[120px] sm:rounded-sm"
+              imageSize="w-full h-full max-h-[245px] rounded-none
+              sm:max-w-[400px] sm:rounded-tl-none sm:rounded-bl-none sm:rounded-sm"
               cardDescriptionStyles="leading-tight text-sm sm:w-3/4"
-              cardHeaderStyles='w-full h-auto p-3 sm:text-lg lg:text-xl'
-              className='p-2 w-full h-full flex-col flex justify-start items-center 
-              pb-0 rounded-none sm:p-4 sm:flex-row-reverse'
+              cardHeaderStyles='w-full h-auto p-4 sm:text-lg lg:text-xl sm:px-4 sm:py-6'
+              className='w-full h-full flex-col flex justify-start items-center 
+              sm:flex-row-reverse'
             />
           ))}
         </div>
