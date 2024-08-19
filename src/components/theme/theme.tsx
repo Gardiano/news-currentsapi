e049cd6d88061bbe0aaa01c3eccfee6f6ee53c11
Gardiano <joaopaulong@hotmail.com>
2024-08-19 10:49:58 -0300

    style(theme.tsx): add vertical padding

--- src/components/theme/theme.tsx
+++ src/components/theme/theme.tsx
@@ -6,7 +6,7 @@ export const Theme = (props: ThemeProps) => {
   return (
     <>{props.theme &&
       <h1 className="w-full max-w-[1144px] h-auto mx-auto flex justify-center
-        font-times text-2xl p-6 capitalize sm:text-4xl">
+        font-times text-2xl py-6 capitalize sm:py-14 sm:text-4xl">
         {props.theme}
       </h1>}
     </>
