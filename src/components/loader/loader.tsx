a2cc2a787817189e4ce7c9d227fc5431b5668509
Gardiano <joaopaulong@hotmail.com>
2024-08-19 14:50:41 -0300

    style(loader.tsx): add margin

--- src/components/loader/loader.tsx
+++ src/components/loader/loader.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "../ui/skeleton"
 
 export const Loader = () => {
   return (
-    <div className="w-full max-w-[1144px] h-full my-6 mx-auto gap-4 flex flex-col items-center justify-center">
+    <div className="w-full max-w-[1144px] h-full my-6 mx-auto gap-4 flex flex-col mt-20 z-10 items-center justify-center">
       {Array.from({ length: 4 }).map((_, i) => (
         <Skeleton key={i} className='w-[calc(100%-20px)] h-[257px] bg-slate-400 rounded-none' />
       ))}
