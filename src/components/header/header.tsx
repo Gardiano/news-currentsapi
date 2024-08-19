15f67a5aaa313cc2ecf4f8f9d8e005b9cb6984c4
Gardiano <joaopaulong@hotmail.com>
2024-08-19 10:32:40 -0300

    refactor(header.tsx): change url value in NavigationLink component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,25 +1,29 @@
-import { NavigationLink } from "../navigation/navigationLink"
-import bg from '../../assets/logo4.jpg'
+import { NavigationLink } from "../navigation/navigationLink";
+
 export const Header = () => {
-  // const links = ['politics', 'sports', 'tech', 'entertainment']
+  
+  const links = ['world', 'politics', 'sports', 'entertainment'];
+
   return (
-    <header className="w-full h-[85px] flex flex-row items-center justify-center 
-    ">
+    <header className="w-full h-full py-4 flex flex-col items-center justify-center bg-zinc-900">
       <nav className="w-[calc(100%-28px)] max-w-[1110px] h-full flex flex-row items-center">
         <div className="w-full max-w-1/2 h-full flex items-center">
-          <NavigationLink className="flex flex-row gap-2 w-fit h-fit text-2xl font-times" url="/">
-            <img src={bg} alt="logo" className=" w-[80px] h-[30px]" /> News
+          <NavigationLink
+            url="/news"
+            className="text-slate-100 flex flex-row gap-2 w-fit h-fit text-2xl font-times">
+            News
           </NavigationLink>
         </div>
-        {/* <div className="w-full max-w-1/2 h-full flex items-center justify-end gap-8">
+        <div className="hidden w-full max-w-1/2 h-full items-center justify-end gap-8 sm:flex">
           {links.map((link, index) => (
             <NavigationLink
               key={index}
-              className="text-lg font-bold" url={`/${link}`}>
+              url={`/news/${link}`}
+              className='text-sm font-prata capitalize text-slate-100'>
               {link}
             </NavigationLink>
           ))}
-        </div> */}
+        </div>
       </nav>
     </header>
   )
