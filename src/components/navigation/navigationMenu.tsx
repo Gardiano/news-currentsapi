f0198a853448bd9d0fc2507bd4f0827f997b5521
Gardiano <joaopaulong@hotmail.com>
2024-08-19 10:48:43 -0300

    style(navigationMenu.tsx): add a border effect when a theme is selected and a drop shadow effect at the end of the carousel

--- src/components/navigation/navigationMenu.tsx
+++ src/components/navigation/navigationMenu.tsx
@@ -1,29 +1,46 @@
 import { useNavigate } from "react-router-dom";
 import { Carousel, CarouselContent } from "../ui/carousel";
 import { NavigationLink } from "./navigationLink";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export const NavigationMenu = () => {
+  const routes = useParams();
   const navigate = useNavigate();
+  const [routeValue, setRouteValue] = useState('world');
 
   const handleNavigation = (theme: string) => {
     navigate(`/news/${theme}`);
   };
 
-  const themes = [
-    "world", "politics", "regional", "business", "finance", "sports", "technology",
-    "lifestyle", "science", "entertainment", "health", "opinion", "food",
+  const themes: string[] = [
+   "latest news", "world", "politics", "regional", "business", "finance", "sports", "technology",
+    "lifestyle", "science", "entertainment", "opinion", "food",
     "fashion", "travel", "economy", "environment", "art", "music", "movie"
   ];
 
+  useEffect(() => {
+    focus();
+  }, [routes]);
+
+  const focus = () => {
+    const route_value = routes.id;
+    const filtered = themes.filter((theme => theme === route_value));
+    if (filtered)
+      setRouteValue(filtered[0]);
+  }
+
   return (
-    <nav className="w-[calc(100%-38px)] max-w-[1080px] mx-auto h-fit flex border-b border-b-slate-900 mb-4">
-      <Carousel className="w-full h-fit mx-auto overflow-x-auto"
-        style={{ scrollBehavior:'smooth'}}>
+    <nav className='w-[calc(100%-38px)] max-w-[1144px] mx-auto h-fit flex 
+      border-b border-b-slate-900 mb-4 bg-white rounded-t-sm pl-1'>
+      <Carousel className="w-full h-fit mx-auto overflow-x-auto carousel-shadow-left"
+        style={{ scrollBehavior: 'smooth' }}>
         <CarouselContent className="pl-2">
           {themes.map((theme, index) => (
             <NavigationLink
               key={index}
-              className="flex-shrink-0 h-fit font-times text-slate-600 px-2 py-1 mx-1"
+              className={`flex-shrink-0 h-fit font-times text-black px-2 py-1 mx-1
+              ${routeValue === theme ? 'border-b-2 border-black' : ''}`}
               onClick={() => handleNavigation(theme)}>
               {theme}
             </NavigationLink>
