7bbc07ba062ed08c023336fc4f9429cc2e910dc8
Gardiano <joaopaulong@hotmail.com>
2024-08-19 10:54:14 -0300

    chore(App.tsx): add context provider to routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,15 @@
 import { Outlet } from 'react-router-dom'
 import { PrimeReactProvider } from 'primereact/api';
 import { Header } from './components/header/header';
-import { NavigationMenu } from './components/navigation/navigationMenu';
+import { NewsContextProvider } from './components/context/NewsContext';
 
 export function App() {
   return (
-      <div>
-        <PrimeReactProvider>
-          <Header />
-          {/* <NavigationMenu /> */}
-          <Outlet />
-        </PrimeReactProvider>
-      </div>
+    <PrimeReactProvider>
+      <Header />
+      <NewsContextProvider>
+        <Outlet />
+      </NewsContextProvider>
+    </PrimeReactProvider>
   )
 }
