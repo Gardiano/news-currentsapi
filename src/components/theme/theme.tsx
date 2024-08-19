46361dbb17876cd99e20d2990a33e235e721594f
Gardiano <joaopaulong@hotmail.com>
2024-08-19 14:56:18 -0300

    style(theme.tsx): add padding

--- src/components/theme/theme.tsx
+++ src/components/theme/theme.tsx
@@ -5,8 +5,8 @@ interface ThemeProps {
 export const Theme = (props: ThemeProps) => {
   return (
     <>{props.theme &&
-      <h1 className="w-full max-w-[1144px] h-auto mx-auto flex justify-center
-        font-times text-2xl py-6 capitalize sm:py-14 sm:text-4xl">
+      <h1 className="hidden w-full max-w-[1144px] h-auto mx-auto justify-center
+        font-times text-2xl py-6 capitalize sm:pt-24 sm:text-4xl sm:flex">
         {props.theme}
       </h1>}
     </>
