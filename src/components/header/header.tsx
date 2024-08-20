292bdaf8e65b9bbcac80ddfcfe1e4d02e523fe57
Gardiano <joaopaulong@hotmail.com>
2024-08-20 13:25:36 -0300

    feat(header.tsx): add sheetMenu component and Search component at Header component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,30 +1,40 @@
+
+import { SheetMenu } from "../menu/menu";
 import { NavigationLink } from "../navigation/navigationLink";
+import { Search } from "../search/search";
 
 export const Header = () => {
 
   const links = ['world', 'politics', 'sports', 'entertainment'];
-  
+
   return (
-    <header className="w-full h-[70px] fixed top-0 py-4 flex flex-col items-center justify-center bg-zinc-900 z-50">
-      <nav className="w-[calc(100%-28px)] max-w-[1110px] h-full flex flex-row items-center">
-        <div className="w-full max-w-1/2 h-full flex items-center">
+    <header className="w-full h-[58px] fixed top-0 py-4 flex flex-col items-center justify-center bg-zinc-900 z-50">
+    <nav className="w-full h-full flex items-center justify-between 
+    sm:w-[calc(100%-28px)] sm:max-w-[1110px]">
+      <div className="flex items-center absolute left-0">
+        <SheetMenu />
+      </div>
+      <div className="flex justify-center w-[30%] h-full sm:justify-start">
+        <NavigationLink
+          url="/news"
+          className="text-slate-100 flex flex-row gap-2 text-xl font-times text-center sm:text-2xl">
+          NEWS
+        </NavigationLink>
+      </div>
+      <div className="w-full h-[58px] flex flex-row items-center">
+        <Search />
+      </div>
+      <div className="hidden sm:flex w-auto max-w-1/2 h-full items-center justify-end gap-8">
+        {links.map((link, index) => (
           <NavigationLink
-            url="/news"
-            className="text-slate-100 flex flex-row gap-2 w-fit h-fit text-2xl font-times">
-            News
+            key={index}
+            url={`/news/${link}`}
+            className="text-sm font-prata capitalize text-slate-100">
+            {link}
           </NavigationLink>
-        </div>
-        <div className="hidden w-full max-w-1/2 h-full items-center justify-end gap-8 sm:flex">
-          {links.map((link, index) => (
-            <NavigationLink
-              key={index}
-              url={`/news/${link}`}
-              className='text-sm font-prata capitalize text-slate-100'>
-              {link}
-            </NavigationLink>
-          ))}
-        </div>
-      </nav>
-    </header>
+        ))}
+      </div>
+    </nav>
+  </header>
   )
 }
