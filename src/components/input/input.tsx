4d8835daa7d585a616ef2a9494140a841e6c5f7d
Gardiano <joaopaulong@hotmail.com>
2024-08-20 13:26:18 -0300

    feat(input.tsx): create input component

---
+++ src/components/input/input.tsx
@@ -0,0 +1,22 @@
+import { ChangeEvent, HTMLInputTypeAttribute } from "react";
+import { Input } from "../ui/input";
+
+interface InputProps {
+  placeholder: string
+  type: HTMLInputTypeAttribute
+  value: string
+  className: string
+  onChange: (e: any) => void
+}
+
+export const Inputs = (props: InputProps) => {
+  return (
+      <Input
+      type={props.type}
+      value={props.value}
+      className={props.className}
+      placeholder={props.placeholder}
+      onChange={props.onChange}
+    />
+  )
+}
