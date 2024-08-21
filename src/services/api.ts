17f21457073db4c25c2ea4af27e508f8341975b6
Gardiano <joaopaulong@hotmail.com>
2024-08-21 12:39:26 -0300

    refactor(api.tsx): rename getnews to getNewsByCategory

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,8 +3,7 @@ import { News, NewsProps } from '@/components/models/news';
 import { api } from './baseUrl';
 const key = import.meta.env.VITE_REACT_APP_API_KEY;
 
-
-export const getNews = async (category: string, page_number: number, page_size: number): Promise<NewsProps<News>> => {
+export const getNewsByCategory = async (category: string, page_number: number, page_size: number): Promise<NewsProps<News>> => {
   return new Promise((resolve, reject) => {
     api.get(`/search?domain=theguardian.com&type=1&page_number=${page_number}&page_size=${page_size}&category=${category}&language=en&apiKey=${key}`)
       .then(response => resolve(response))
