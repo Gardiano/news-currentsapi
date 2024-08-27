a45a1a14c358ce103187f161105fe05b8cbdb725
Gardiano <joaopaulong@hotmail.com>
2024-08-27 18:32:44 -0300

    feat(routes.tsx): add routes for searchedNews component

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { Home } from './components/pages/home.tsx';
 import {App} from './App';
+import { SearchedNews } from './components/pages/searchedNews.tsx';
 
 const router = createBrowserRouter([
   {
@@ -9,7 +10,8 @@ const router = createBrowserRouter([
     children: [
       { index: true, element: <Home /> },
       { path: 'news', element: <Home />  },
-      { path: 'news/:id',  element: <Home /> }
+      { path: 'news/:id',  element: <Home /> },
+      { path: 'news/searched/:id',  element: <SearchedNews /> }
     ],
   },
   { path: "*", element: <h1>error;</h1> }
