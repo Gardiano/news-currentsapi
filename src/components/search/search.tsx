5a6a55290f94993ae23c690182208533c8b9360a
Gardiano <joaopaulong@hotmail.com>
2024-08-27 18:30:32 -0300

    feat(search.tsx): add search component

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,33 +1,36 @@
-import { ChangeEvent, useState } from "react"
 import { Inputs } from "../input/input"
 import { SearchIcon } from "lucide-react";
 import { Button } from "../ui/button";
+import { NavigationLink } from "../navigation/navigationLink";
+import { useSearch } from "../hooks/useSearch";
 
 export const Search = () => {
-  const [value, setValue] = useState<string>('');
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    setValue(value);
-  };
+  const { value, handleChange } = useSearch();
 
   return (
-    // adicionar um sheet component para renderizar o search component;
-    <div className="w-full h-[50px] flex flex-row items-center justify-end sm:justify-center">
-      <Inputs
-        className="hidden w-full max-w-[400px] h-[34px] 
-        border-0 border-b border-zinc-700 bg-zinc-800 rounded-xs text-white sm:flex"
-        type='text'
-        value={value}
-        onChange={handleChange}
-        placeholder="Search..."
-      />
-      <Button
-        onClick={() => { console.log('aaa') } }
-        className="h-[34px] mr-2 bg-transparent rounded-tl-none rounded-bl-none 
-        hover:bg-transparent sm:bg-zinc-600">
-        <SearchIcon />
-      </Button>
+    <div className="w-full max-w-[1144px] h-full flex flex-row justify-center mb-4">
+      <div className="w-full h-fit flex flex-row justify-center items-center">
+        <Inputs
+          className="w-full h-[36px] flex bg-zinc-300 border-0 border-b border-zinc-400
+          rounded-none text-zinc-900 shadow-none sm:h-[42px]"
+          type='text'
+          value={value}
+          onChange={handleChange}
+          placeholder="Searching by keywords"
+        />
+
+        <Button
+          disabled={value?.length < 4}
+          className="w-[64px] h-[36px] m-0 p-0 bg-zinc-600 rounded-tl-none rounded-bl-none hover:bg-zinc-600 sm:h-[42px]">
+          <NavigationLink
+            url={`/news/searched/${value}`}
+            className="w-full h-full flex items-center justify-center text-white 
+              text-xs bg-transparent
+             sm:bg-zinc-600 rounded-none sm:rounded-tr-sm sm:rounded-br-sm">
+            <SearchIcon className="w-[44px] h-[16px] sm:bg-zinc-600" />
+          </NavigationLink>
+        </Button>
+      </div>
     </div>
   )
 }
