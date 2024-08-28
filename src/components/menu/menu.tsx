73604025e34ccbc365a40a6d88ebcc9f65554146
Gardiano <joaopaulong@hotmail.com>
2024-08-28 11:54:37 -0300

    style(menu.tsx): adjust width, height, and margin-left for layout improvement

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -2,7 +2,6 @@ import { Menu } from "lucide-react";
 import { Button } from "../ui/button";
 import { Sheet, SheetClose, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "../ui/sheet";
 import { Select, SelectContent, SelectTrigger, SelectValue } from "@/components/ui/select"
-
 import { NavigationLink } from "../navigation/navigationLink";
 
 export const SheetMenu = () => {
@@ -15,7 +14,7 @@ export const SheetMenu = () => {
   ];
 
   return (
-    <nav className="flex flex-col w-[10%] h-fit sm:hidden">
+    <nav className="flex flex-col w-[10%] h-fit">
       {SHEET_SIDES.map((side) => (
         <Sheet key={side}>
           <SheetTrigger asChild>
@@ -26,26 +25,26 @@ export const SheetMenu = () => {
           <SheetContent side={side}
             className="bg-[#171719] py-4 px-0 text-white">
             <SheetHeader>
-              <SheetTitle className="text-slate-100 font-times text-sm">NEWS</SheetTitle>
+              <SheetTitle className="text-slate-100 ml-2 font-times text-sm">NEWS</SheetTitle>
             </SheetHeader>
-            <div className="h-full grid py-8 w-full">
+            <div className="h-fit grid py-8 w-full">
               <SheetTrigger asChild>
                 <>
                   <Select>
                     <SelectTrigger
-                      className="w-[99%] border-0 border-t border-b border-zinc-600 rounded-none">
+                      className="w-[98%] my-2 border-0 border-t border-b border-zinc-600 rounded-none">
                       <SelectValue placeholder="News" />
                     </SelectTrigger>
                     <SelectContent dir="ltl"
-                      className="bg-[#171719] text-white flex flex-col flex-wrap w-[80%] border-none">
+                      className="bg-[#171719] text-white flex flex-col flex-wrap w-[90%] border-none">
                       {themes.map(theme => (
                         <div key={theme}>
                           <SheetClose
                             asChild
-                            className="w-auto h-[30px] flex flex-col flex-wrap border-b border-zinc-400">
+                            className="w-full h-[30px] flex flex-col flex-wrap border-b border-zinc-400">
                             <NavigationLink
                               url={`/news/${theme}`}
-                              className="w-fit text-white text-sm font-thin 
+                              className="w-full text-white text-sm font-thin 
                               capitalize border-b border-zinc-600 pl-2 pt-2">
                               {theme}
                             </NavigationLink>
