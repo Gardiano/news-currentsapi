c5f378c16f1b25dd6aeab6dfbdb92840645ad136
Gardiano <joaopaulong@hotmail.com>
2024-09-03 13:52:10 -0300

    feat(search.tsx): add spin loader svg

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,14 +1,14 @@
 import { Inputs } from "../input/input"
-import { SearchIcon } from "lucide-react";
+import { Loader, SearchIcon } from "lucide-react";
 import { Button } from "../ui/button";
 import { NavigationLink } from "../navigation/navigationLink";
 import { useSearch } from "../hooks/useSearch";
 
 export const Search = () => {
-  const { value, handleChange } = useSearch();
+  const { value, handleChange, loading } = useSearch();
 
   return (
-    <div className="w-full max-w-[1144px] h-full hidden flex-row justify-center mb-4 lg:flex">
+    <div className="w-full max-w-[1144px] h-full hidden flex-row justify-center lg:flex">
       <div className="w-[calc(100%-40px)] h-fit flex flex-row justify-center items-center">
         <Inputs
           className="w-full h-[36px] flex bg-zinc-300 border-0 border-b border-zinc-400
@@ -21,13 +21,14 @@ export const Search = () => {
 
         <Button
           disabled={value?.length < 4}
-          className="w-[64px] h-[36px] m-0 p-0 bg-zinc-600 rounded-tl-none rounded-bl-none hover:bg-zinc-600 sm:h-[42px]">
+          className="w-[64px] h-[36px] m-0 p-0 bg-zinc-600 
+          rounded-tl-none rounded-bl-none hover:bg-zinc-600 sm:h-[42px]">
           <NavigationLink
             url={`/news/searched/${value}`}
             className="w-full h-full flex items-center justify-center text-white 
               text-xs bg-transparent
              sm:bg-zinc-600 rounded-none sm:rounded-tr-sm sm:rounded-br-sm">
-            <SearchIcon className="w-[44px] h-[16px] sm:bg-zinc-600" />
+              {!loading ? <SearchIcon className="w-[44px] h-[16px] sm:bg-zinc-600" /> : <Loader className="animate-spin" />}
           </NavigationLink>
         </Button>
       </div>
