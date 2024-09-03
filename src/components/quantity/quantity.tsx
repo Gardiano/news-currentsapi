3d0f3b1b967264732f5463af6ab217fda204d1b4
Gardiano <joaopaulong@hotmail.com>
2024-09-03 13:50:38 -0300

    styles(quantity.tsx): add enhancements

--- src/components/quantity/quantity.tsx
+++ src/components/quantity/quantity.tsx
@@ -5,8 +5,8 @@ interface QuantityProps {
 
 export const Quantity = (props: QuantityProps) => {
   return (
-    <span className="w-full h-auto flex flex-row items-center justify-center bg-sky-100 text-xs 
-    text-sky-900 my-0 p-2 gap-1 lg:h-[30px]">
+    <span className="w-full h-auto flex flex-row items-center justify-center 
+    text-xs my-0 p-2 gap-1 lg:h-[50px]">
      {props.quantity} news results for: <u> {props.id} </u>
     </span>
   )
