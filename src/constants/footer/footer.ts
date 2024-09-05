5df32d4b3b8e27e532fd4b64dec431d89048671a
Gardiano <joaopaulong@hotmail.com>
2024-09-05 14:01:40 -0300

    feat(constants/footer.ts): add arrays to serve the footer component

---
+++ src/constants/footer/footer.ts
@@ -0,0 +1,19 @@
+export const getInTouch = [
+  "Help",
+  "About us",
+  "Contact us",
+  "Careers",
+  "Display advertising",
+  "Place an announcement",
+  "Classified advertising",
+  "The Times corrections",
+  "The Sunday Times corrections",
+  "The Times Editorial Complaints",
+  "The Sunday Times Editorial Complaints",
+];
+
+export const fromNews = [
+  "The Times e-paper", "The Sunday Times e-paper", "The Sunday Times Wine Club",
+  "Times Print Gallery", "The Times Archive", "Times Crossword Club", "Sunday Times Driving",
+  "Times+", "The Sunday Times Rich List", "Travel", "Good University Guide", "Schools Guide"
+];
