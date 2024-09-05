5eb5c0049f66d755d75a4867d61f5b413f1eb680
Gardiano <joaopaulong@hotmail.com>
2024-09-05 14:02:13 -0300

    feat(App.tsx): add footer component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { PrimeReactProvider } from 'primereact/api';
 import { Header } from './components/header/header';
 import { NewsContextProvider } from './components/context/NewsContext';
 import { SearchContextProvider } from './components/context/SearchContext';
+import { Footer } from './components/footer/footer';
 
 export function App() {
   return (
@@ -16,6 +17,7 @@ export function App() {
           <Outlet />
         </SearchContextProvider>
       </NewsContextProvider>
+      <Footer />
     </PrimeReactProvider>
   )
 }
