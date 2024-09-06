32c728df7e3f69a96f9dcbe706473a99706f88bf
Gardiano <joaopaulong@hotmail.com>
2024-09-06 13:19:02 -0300

    styles(footer.tsx): styles enhancements

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -6,15 +6,14 @@ export const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
     <footer className="w-full h-full flex flex-row justify-center bg-zinc-900 text-slate-100">
-      <div className="w-full max-w-[1144px] h-auto mx-auto py-4 flex flex-col p-4">
+      <div className="w-full max-w-[1144px] h-full mx-auto py-4 flex flex-col p-4">
         <h1 className='font-times'>NEWS</h1>
-
-        <div className="w-full h-auto grid grid-cols-1 lg:grid-cols-2 gap-4 
+        <div className="w-full h-full grid grid-cols-1 lg:grid-cols-2 gap-4 
           text-xs text-slate-200 font-thin py-4">
           <div className="flex flex-col items-start justify-start gap-4 lg:flex-row">
             <div className="w-full flex flex-wrap items-center">
               <Separator orientation='horizontal' className='border-1 bg-slate-400' />
-              <h1 className='h-fit font-times py-2 text-lg'>Get in touch</h1>
+              <h1 className='h-fit font-times py-2 text-md'>Get in touch</h1>
               <Separator orientation='horizontal' className='border-1 bg-slate-400' />
               <div className="w-1/2 h-auto flex items-start flex-col gap-4 py-4">
                 {getInTouch.slice(0, 6).map(link => (
@@ -36,7 +35,7 @@ export const Footer = () => {
           <div className="flex flex-col items-start justify-start gap-4 lg:flex-row">
             <div className="w-full flex flex-wrap justify-between">
             <Separator orientation='horizontal' className='border-1 bg-slate-400' />
-              <h1 className='font-times py-2 text-lg'>More from news</h1>
+              <h1 className='h-fit font-times py-2 text-md'>More from news</h1>
               <Separator orientation='horizontal' className='border-1 bg-slate-400' />
               <div className="w-1/2 h-full flex flex-col gap-4 py-4">
                 {fromNews.slice(0, 6).map(link => (
@@ -56,10 +55,11 @@ export const Footer = () => {
           </div>
         </div>
         <Separator orientation='horizontal' className='border-1 bg-slate-400' />
-        <span className='w-full h-fit flex justify-center  text-xs gap-1 py-4 font-thin'> © News <b> {currentYear} </b> </span>
+        <span className='w-full h-fit flex justify-center  text-xs gap-1 py-4 font-thin'> 
+          © News <b> {currentYear} </b> 
+        </span>
         <Separator orientation='horizontal' className='border-1 bg-slate-400' />
       </div>
-      
     </footer>
   )
 }
